da755f79f8a755e44e52c3493a17e5a9c7a218f2
Jonny Danger <mpjovanovich@gmail.com>
2024-10-26 20:58:18 -0400

    fixed type not set to module; added some test files - can debug now

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+import fs from "fs";
+
+const markdownPath =
+  "/home/mpjovanovich/git/pretty-mark/test/sample_markdown/test.md";
+
+const markdown = fs.readFileSync(markdownPath, "utf8");
+
+console.log(markdown);
+
+// console.log("Hello, world!");
